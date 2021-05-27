df282a4cce15f0e1f918c07f7bec6fe696a076ef
Dushyant Pawar <pawardushyant22@gmail.com>
2021-05-28 01:13:11 +0530

    first commit

---
+++ xcframework/ToastMakerVariation.xcframework/ios-x86_64-simulator/ToastMakerVariation.framework/Headers/ToastMakerVariation.h
@@ -0,0 +1,19 @@
+//
+//  ToastMakerVariation.h
+//  ToastMakerVariation
+//
+//  Created by Apple on 27/08/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ToastMakerVariation.
+FOUNDATION_EXPORT double ToastMakerVariationVersionNumber;
+
+//! Project version string for ToastMakerVariation.
+FOUNDATION_EXPORT const unsigned char ToastMakerVariationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToastMakerVariation/PublicHeader.h>
+
+
